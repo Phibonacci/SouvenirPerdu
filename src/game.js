b31e71d93745940062f3e2aa919d827eec78400b
Paul-Maxime <github@paulmaxime.fr>
2021-01-30 02:01:17 +0100

    Implement a basic camera movement using the mouse

--- src/game.js
+++ src/game.js
@@ -6,6 +6,10 @@ export default class Game extends Phaser.Game {
 			type: Phaser.AUTO,
 			width: 1024,
 			height: 600,
+			scale: {
+				mode: Phaser.Scale.FIT,
+				autoCenter: Phaser.Scale.CENTER_BOTH,
+			},
 			scene: [RoomScene],
 		});
 	}
--- src/scenes/room-scene.js
+++ src/scenes/room-scene.js
@@ -5,13 +5,31 @@ export default class RoomScene extends Phaser.Scene {
 
 	preload() {
 		console.log("Preloading assets...");
+
+		this.load.image("background", "assets/tests/background.png");
 	}
 
 	create() {
 		console.log("Creating the game...");
+
+		this.background = this.add.sprite(0, 0, "background");
+		this.background.setOrigin(0, 0);
+		this.cameras.main.setBounds(0, 0, 1920, 1080);
+
+		this.expectedZoom = 1.0;
+		this.cameras.main.zoom = this.expectedZoom;
+
+		this.input.on("pointerdown", () => {
+			this.expectedZoom = this.expectedZoom > 2 ? 1.0 : 3.0;
+		});
 	}
 
-	update(_, elapsed) {
-		"";
+	update() {
+		this.cameras.main.centerOn(
+			(game.input.mousePointer.x * 1920) / 1024,
+			(game.input.mousePointer.y * 1080) / 600
+		);
+
+		this.cameras.main.zoom = (this.expectedZoom + this.cameras.main.zoom) / 2;
 	}
 }
