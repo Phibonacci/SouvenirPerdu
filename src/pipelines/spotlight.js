73e8fc41121aba013a4add7466022fc7c81b9bb2
Paul-Maxime <github@paulmaxime.fr>
2021-01-31 07:45:43 +0100

    Increase the ambiant light, I can't see anything

--- src/pipelines/spotlight.js
+++ src/pipelines/spotlight.js
@@ -11,7 +11,7 @@ vec3 makeCircle(vec2 st,vec2 center, vec3 col){
 	float d = distance(st,center);
 	float pct = smoothstep(r,r+0.1,d);
 	vec3 v = vec3(1.0-pct)*col;
-	return clamp(v, 0.01, 1.0);
+	return clamp(v, 0.05, 1.0);
 }
 void main(void) {
 	vec4 color = texture2D(uMainSampler, outTexCoord);
