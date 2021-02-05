4d24652147f32fae8d6be5f45fc6692920e6d88c
Jean Fauquenot <jean.fauquenot@gmail.com>
2021-02-05 08:02:51 +0100

    Fix: oh, that was actually two files

---
+++ src/entities/videotape.js
@@ -0,0 +1,26 @@
+export default class Videotape extends Phaser.GameObjects.Sprite {
+	constructor(scene, x, y) {
+		super(scene, x, y, "videotape");
+
+		this.scale = 0.1;
+		this.setInteractive({ useHandCursor: true });
+
+		this.on("pointerover", () => {
+			this.tint = 0x808080;
+		});
+
+		this.on("pointerout", () => {
+			this.tint = 0xffffff;
+		});
+
+		this.on("pointerdown", (pointer) => {
+			if (pointer.leftButtonDown()) {
+				this.emit("selected");
+			}
+		});
+	}
+
+	onSelected() {}
+
+	onUnselected() {}
+}
