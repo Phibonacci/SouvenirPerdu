e3e302a213a155fa6b173aafd12e2f35a6eff762
Jean Fauquenot <jean.fauquenot@gmail.com>
2021-02-10 21:29:46 +0100

    Add recorder

---
+++ src/entities/recorder.js
@@ -0,0 +1,43 @@
+export default class Recorder extends Phaser.GameObjects.Sprite {
+	constructor(scene, x, y) {
+		super(scene, x, y, "recorder-off");
+
+		this.scale = 0.332;
+		this.zoomFactor = 2.9;
+
+		this.setInteractive({ useHandCursor: true });
+
+		this.on("pointerover", () => {
+			this.tint = 0x808080;
+		});
+
+		this.on("pointerover", () => {
+			this.tint = 0xc0c0c0;
+		});
+
+		this.on("pointerout", () => {
+			this.tint = 0xffffff;
+		});
+
+		this.on("pointerup", (pointer) => {
+			if (pointer.button === 0) {
+				if (this.isSelected) {
+					this.emit("selected");
+					if (this.texture.key == "recorder-off") {
+						this.setTexture("recorder-on");
+					}
+				} else {
+					this.emit("selected");
+				}
+			}
+		});
+	}
+
+	onSelected() {
+		this.isSelected = true;
+	}
+
+	onUnselected() {
+		this.isSelected = false;
+	}
+}
